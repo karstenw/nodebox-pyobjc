17a4baa474d09715e3a3056d317a93befb8d362d
Karsten Wolf <karstenwo@web.de>
2017-05-20 22:12:18 +0200

    Applied the shoebot colors library patch and added zipfile import.

--- macboot.py
+++ macboot.py
@@ -3,6 +3,30 @@
 # This just imports everything from the nodebox.gui.mac module
 # and works from there
 
+# for the libraries
+import sgmllib
+import bs4
+import numpy
+import sqlite3
+import zipfile
+
+import hashlib
+import base64
+import re
+import pickle
+import UserList
+
+
+# PIL / Pillow support
+import PIL
+import PIL.Image
+import PIL.ImageFilter
+import PIL.ImageChops
+import PIL.ImageEnhance
+import PIL.ImageOps
+import PIL.ImageDraw
+import PIL.ImageStat
+
 
 import objc
 import Foundation
@@ -18,29 +42,6 @@ import nodebox.graphics
 #import nodebox.graphics.cocoa
 import nodebox.gui
 import nodebox.gui.mac
-
-# for the libraries
-import sgmllib
-import bs4
-import numpy
-import sqlite3
-
-# PIL / Pillow support
-import PIL
-import PIL.Image
-import PIL.ImageFilter
-import PIL.ImageChops
-import PIL.ImageEnhance
-import PIL.ImageOps
-import PIL.ImageDraw
-import PIL.ImageStat
-
-import hashlib
-import base64
-import re
-import pickle
-import UserList
-
 
 #import matplotlib
 
