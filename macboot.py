838dedcf4dc6d1edac589cb2fcf5f953f2d16bcb
Karsten Wolf <karstenwo@web.de>
2017-08-01 12:26:57 +0200

    Reordered some imports.

--- macboot.py
+++ macboot.py
@@ -3,6 +3,10 @@
 # This just imports everything from the nodebox.gui.mac module
 # and works from there
 
+import os
+import operator
+import warnings
+
 # for the libraries
 import sgmllib
 import bs4
@@ -15,6 +19,28 @@ import base64
 import re
 import pickle
 import UserList
+
+# needed by matplotlib et al.
+import six
+import pyparsing
+import cycler
+import requests
+import pymongo
+import dateutil
+import copy
+import csv
+import functools
+import itertools
+import gzip
+import io
+import httplib
+import http
+import http.client
+import http.server
+import urllib
+import urlparse
+import decimal
+import gettext
 
 
 # PIL / Pillow support
@@ -39,15 +65,6 @@ import nodebox.geo
 import nodebox.graphics
 import nodebox.gui
 import nodebox.gui.mac
-
-# needed by matplotlib
-import six
-import pyparsing
-import cycler
-import requests
-import pymongo
-import dateutil
-
 
 
 # Do not import these here; they are in the app and are imported by scripts
