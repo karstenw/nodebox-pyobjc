055e50141b49229c0c98f60aa418f149e74fa2b2
Karsten Wolf <karstenwo@web.de>
2017-08-15 13:42:09 +0200

    Added missing keywords for syntax coloring: MOUSEX, MOUSEY, mousedown, keydown, key, scrollwheel, wheeldelta.

--- nodebox/PyFontify.py
+++ nodebox/PyFontify.py
@@ -40,6 +40,8 @@ keywordsList += ["None", "True", "False"]
 keywordsList += graphics.__all__
 keywordsList += util.__all__
 keywordsList += dir(graphics.Context)
+keywordsList += ["MOUSEX", "MOUSEY", "mousedown", "keydown", "key", "scrollwheel", "wheeldelta"]
+
 
 # Build up a regular expression which will match anything
 # interesting, including multi-line triple-quoted strings.
