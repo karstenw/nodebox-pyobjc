8b406ac4f86152d16134c33026b2c6d3b02e6c96
Karsten Wolf <karstenwo@web.de>
2020-07-26 14:53:53 +0200

    Update setup.py

--- libs/bwdithering/setup.py
+++ libs/bwdithering/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup, Extension
 
-atkinsondither = Extension("bwdither", sources = ["bwdithering.c"])
+bwdither = Extension("bwdither", sources = ["bwdithering.c"])
 
 setup (name = "bwdither",
        version = "0.1",
        author = "Karsten Wolf",
        description = "Atkinson, Floyd-Steinberg and other ditherings of grayscale images.",
-       ext_modules = [bwdithering])
+       ext_modules = [bwdither])
 
