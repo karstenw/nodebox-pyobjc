47448162f5bc62d3a3d2f3795143503d43ae6de9
Karsten Wolf <karstenwo@web.de>
2020-07-26 14:50:39 +0200

    Added fractal.c, bwdithering.c

--- setup_console.py
+++ setup_console.py
@@ -60,7 +60,8 @@ The current version features:
 
 
 ext_modules=[
-    Extension('atkinsondither', ['libs/atkinsondither/atkinsondither.c']),
+    Extension('bwdithering', ['libs/bwdithering/bwdithering.c']),
+    Extension('fractal', ['libs/fractal/fractal.c']),
     Extension('cGeo', ['libs/cGeo/cGeo.c']),
     Extension('cPathmatics', ['libs/pathmatics/pathmatics.c']),
     Extension('cPolymagic', ['libs/polymagic/gpc.c', 'libs/polymagic/polymagic.m'],
