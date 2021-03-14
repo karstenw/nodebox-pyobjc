7a11facb9f1bc081f6398603dc63e3b13067c159
Karsten Wolf <karstenwo@web.de>
2021-03-14 17:03:05 +0100

    Update Example fractal 1.py

--- examples/New Functions/Example fractal 1.py
+++ examples/New Functions/Example fractal 1.py
@@ -1,3 +1,6 @@
+import math
+import time
+
 import pprint
 pp = pprint.pprint
 
@@ -5,10 +8,34 @@ from AppKit import NSBitmapImageRep, NSDeviceRGBColorSpace
 
 colors = ximport("colors")
 
-PUREPYTHON = 0
+
+def hsv_to_rgb(h, s, v):
+    if s == 0.0:
+        v *= 1.0
+        return (v, v, v)
+    i = int( h * 6.) # XXX assume int() truncates!
+    f = ( h * 6.) - i
+    p = float(1.0 * (v * (1. - s)))
+    q = float(1.0 * (v * (1. - s * f)))
+    t = float(1.0 * (v * (1. - s * (1.-f))))
+    v *= 1.0
+    i %= 6
+    if i == 0:
+        return (v, t, p)
+    if i == 1:
+        return (q, v, p)
+    if i == 2:
+        return (p, v, t)
+    if i == 3:
+        return (p, q, v)
+    if i == 4:
+        return (t, p, v)
+    if i == 5:
+        return (v, p, q)
 
 
-def makeColorlookup( iterations ):
+
+def makeColorlookupOLD( iterations, maxi ):
     #clr1 = colors.rgb(0.6, 0.8, 1.0)
     #clr2 = colors.rgb(0.0, 0.2, 0.4)
     #g = colors.gradient(clr1, clr2, steps=iterations)
@@ -32,9 +59,82 @@ def makeColorlookup( iterations ):
     return bytes( colorcache )
 
 
+def paletteBlueish( i, maxi ):
+    log_it = math.log( float(i) )
+    r = int( 255 * (1 + math.cos(3.320 * log_it)) / 2.0 )
+    g = int( 255 * (1 + math.cos(0.974 * log_it)) / 2.0 )
+    b = int( 255 * (1 + math.cos(0.412 * log_it)) / 2.0 )
+    a = 255
+    return r,g,b,a
+
+def paletteOther( i, maxi ):
+    r = (255 - i*30) % 255
+    g = 100
+    b = 100
+    a = 255
+    return r,g,b,a
+
+
+h = 100/255.0
+s = 100/255.0
+
+def paletteHLS( i, maxi ):
+    v = i / float( maxi)
+    r,g,b = hsv_to_rgb( h, s, v )
+    r = int( 255 * r )
+    g = int( 255 * g )
+    b = int( 255 * b )
+    # pp( (i, maxi, r,g,b) )
+    return r,g,b,255
+    
+
+def paletteOrange(i, maxi):
+    h = i / float(maxi)
+    s = i / float(maxi)
+    v = i / float(maxi)
+    r,g,b = hsv_to_rgb( h, s, v )
+    r = int( 255 * r )
+    g = int( 255 * g )
+    b = int( 255 * b )
+    return r,g,b,255
+
+    
+paletteMakers = dict(
+    Old=makeColorlookupOLD,
+    Blueish=paletteBlueish,
+    Other=paletteOther,
+    HLS=paletteHLS)
+    
+
+def makeColorlookup( iterations ):
+    colorcache = bytearray( iterations * 4 )
+    colorcache[0] = colorcache[1] = colorcache[2] = 0
+    colorcache[3] = 255
+    for i in range(1, iterations):
+        idx = i * 4
+        
+        if 0:
+            r,g,b,a = paletteBlueish( i, iterations )
+        elif False:
+            r,g,b,a = paletteHLS( i, iterations )
+        elif True:
+            r,g,b,a = paletteOrange( i, iterations )
+        else:
+            r,g,b,a = paletteOther( i, iterations )
+        colorcache[idx+0] = r
+        colorcache[idx+1] = g
+        colorcache[idx+2] = b
+        colorcache[idx+3] = a
+
+    return bytes( colorcache )
+
+
+
 
 def handlecoordinate(value, name):
-    global fsize, xpos, ypos, delta, iterations, const_real, const_imag, limit, zoom
+    global fsize, xpos, ypos, delta, iterations, const_real
+    global const_imag, limit, zoom, h, s, USE_C_EXT
+
     if name == "fsize":
         fsize = int(value)
     elif name == "xpos":
@@ -49,8 +149,8 @@ def handlecoordinate(value, name):
     elif name == "zoom":
         zoom = float(value)
         delta = 1 / zoom
-        print "delta", delta
-        print "zoom", zoom
+        #print "delta", delta
+        #print "zoom", zoom
     elif name == "iterations":
         iterations = int( value )
     elif name == "const_real":
@@ -61,20 +161,34 @@ def handlecoordinate(value, name):
         #const_real = const_imag
     elif name == "limit":
         limit = float( value )
-    pp( (fsize, xpos, ypos, delta, iterations, const_real, const_imag, limit ) )
+    elif name == "h":
+        h = float( value )
+    elif name == "s":
+        s = float( value )
+    elif name == "USE_C_EXT":
+        USE_C_EXT = bool( value )
+    elif name == "Palette":
+        pass
+    # pp( (fsize, xpos, ypos, delta, iterations, const_real, const_imag, limit ) )
     render( fsize, xpos, ypos, delta, iterations, const_real, const_imag, limit )
 
 
-delta = 4.0
-var("fsize", NUMBER, 400, 300, 1100, handler=handlecoordinate)
-var("xpos", NUMBER, 0.0, -6.0, 6.0, handler=handlecoordinate)
-var("ypos", NUMBER, 0, -3.5, 3.5, handler=handlecoordinate)
+delta = 0.5
+inity = -0.75 # -1.75
+
+var("fsize", NUMBER, 600, 300, 1200, handler=handlecoordinate)
+var("xpos", NUMBER,  0.0, -6.0, 6.0, handler=handlecoordinate)
+var("ypos", NUMBER,  inity, -3.5, 3.5, handler=handlecoordinate)
 # var("delta", NUMBER, 4.0, 0.0001, 20.0, handler=handlecoordinate)
-var("zoom", NUMBER, 0.25, 0.01, 20.0, handler=handlecoordinate)
-var("iterations", NUMBER, 64, 0, 256, handler=handlecoordinate)
+var("zoom", NUMBER, 1.0/delta, 0.01, 5.0, handler=handlecoordinate)
+var("iterations", NUMBER, 100, 0, 1000, handler=handlecoordinate)
 var("const_real", NUMBER, 0.0, 0.0, 2.0, handler=handlecoordinate)
 var("const_imag", NUMBER, 0.0, 0.0, 2.0, handler=handlecoordinate)
-var("limit", NUMBER, 2.0, 0.01, 6.00, handler=handlecoordinate)
+var("limit", NUMBER, 4.0, 0.01, 6.00, handler=handlecoordinate)
+var("h", NUMBER, 0.5,  0.0, 1.00, handler=handlecoordinate)
+var("s", NUMBER, 0.5,  0.0, 1.00, handler=handlecoordinate)
+var("USE_C_EXT", BOOLEAN, True, False, True, handler=handlecoordinate)
+var("Palette", MENU, default="Blueish", handler=handlecoordinate, menuitems=["HLS", "Blueish", "Other", "Old"])
 
 
 def makeImage( pixels, W, H ):
@@ -123,25 +237,29 @@ def iterPixels(width, height, iterations, xpos, dx, ypos, dy, const_real, const_
 
 
 def dofractal( width, height, iterations, xpos, dx, ypos, dy, const_real, const_imag, clut, limit ):
-    result = fractalimage(clut, width,height, iterations,xpos,ypos,dx,dy,const_real,const_imag,limit)
-    return result
+    return fractalimage(clut, width,height, iterations,xpos,ypos,dx,dy,const_real,const_imag,limit)
 
 
 def render(fsize, xpos, ypos, d, i, const_real, const_imag, limit):
+    clut = makeColorlookup( i )
     half = d / 2.0
     left = xpos - half
     top = ypos - half
     W = H = fsize
     size(W, H)
     n = W * H
-    clut = makeColorlookup( i )
-    if PUREPYTHON:
-        pixels = iterPixels(W, H, i, left, d, top, d, const_real, const_imag, clut, limit)
-    else:
+
+    start = time.time()
+
+    if USE_C_EXT:
         pixels = dofractal(W, H, i, left, d, top, d, const_real, const_imag, clut, limit)
-    
+    else:
+        pixels = iterPixels(W, H, i, left, d, top, d, const_real, const_imag, clut, limit)
     bytes = makeImage( pixels, W, H)
     image(None, 0, 0, data=bytes)
+
+    print "%ix%i = %i pixel in %.4fs" % (W,H,W*H,time.time()-start )
+
 
 render(fsize, xpos, ypos, delta, iterations, const_real, const_imag, limit)
 
