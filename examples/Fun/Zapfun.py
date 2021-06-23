2fb014c95a598c0f171110cda08b214472dd57f7
Karsten Wolf <karstenwo@web.de>
2021-06-23 12:02:30 +0200

    Create Zapfun.py

---
+++ examples/Fun/Zapfun.py
@@ -0,0 +1,25 @@
+# Zapfun
+# from
+# https://siafoo.net/snippet/173
+# via https://web.archive.org/web/20110203052513/https://siafoo.net/snippet/173
+
+
+size(1280,900)
+
+fill(0.2)
+rect(0,0,WIDTH,HEIGHT)
+
+fill(1)
+stroke(0.2)
+strokewidth(1)
+font('Zapfino')
+
+for i in range(400):
+    fontsize(random(30,400))
+    rotate(random(360))  
+    chars = 'absdefghijklmnopqrstuvwxyz'
+    p = textpath(choice(chars),
+                 random(-50, WIDTH  + 100),
+                 random(-50, HEIGHT + 100))
+    drawpath(p)
+
