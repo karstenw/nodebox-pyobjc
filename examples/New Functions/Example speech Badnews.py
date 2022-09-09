9a0a6fc4829c395df1849d1f2795f57c3058cec0
Karsten Wolf <karstenwo@web.de>
2022-09-09 16:16:43 +0200

    Relaxed .gitignore

---
+++ examples/New Functions/Example speech Badnews.py
@@ -0,0 +1,10 @@
+
+from __future__ import print_function
+
+if random() > 0.5:
+    print("Bad News")
+    say("ah ah ah ahah ah ah ah ah ah ah ah ah ah ahah ah ah ah ah ah ah ", "BadNews")
+else:
+    print("Good News")
+    say("ah ah ah ahah ah ah ah ah ah ah ah ah ah ahah ah ah ah ah ah ah ", "GoodNews")
+
---
+++ examples/Path/glyphpath_example.py
@@ -0,0 +1,46 @@
+
+from __future__ import print_function
+
+size(0, 0)
+
+import random as rnd
+# Parts taken from beziereditor pathimport example
+
+fonts = fontnames()
+
+fontsize(800)
+
+
+chars = (u"abcdefghijklmnopqrstuvwxyzäöüß"
+         u"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ"
+         u"!§$%&/()=¡¶¢{}ÁÛØ∏ÅÍÏÌÓﬂŒÆ‡ÙÇ÷—")
+for drawing in range(50):
+    chr = choice( chars )
+    f = choice( fonts )
+    x = 100 + (random(WIDTH) - 200)
+    y = HEIGHT - random(400)
+    p = textpath(chr, x, y, font=f)
+    a = random(-90, 90)
+    col = [1,1,random()]
+    rnd.shuffle(col)
+    rnd.shuffle(col)
+    rnd.shuffle(col)
+    print( chr,f )
+    col.append( 0.05 )
+    points = []
+    for pt in p:
+        points.append(pt)
+
+    reset()
+    # rotate(a)
+    background(0,0.2,0.3)
+    for i in range(3):
+        # fill(1, 1, 1, 0.05)
+        fill( *col )
+        stroke(1, 1, 1, 0.1)
+        strokewidth(0.5)
+        scale(0.93)
+        rotate(-i*0.2)
+        translate(i,0)
+        drawpath(points)
+
