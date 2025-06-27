7872cb76f583c375c15acd48f573806cb7bc9292
Karsten Wolf <karstenwo@web.de>
2025-06-27 11:21:05 +0200

    py3 changes

--- libs/polymagic/polymagic_test.py
+++ libs/polymagic/polymagic_test.py
@@ -16,7 +16,7 @@ p4 = NSBezierPath.bezierPathWithOvalInRect_(((72, 72), (100, 100)))
 
 from cPolymagic import *
 
-print intersects(p1, p4)
+print( intersects(p1, p4) )
 
 f = NSFont.fontWithName_size_("Helvetica", 72)
 fp1 = NSBezierPath.bezierPath()
@@ -27,21 +27,21 @@ fp2 = NSBezierPath.bezierPath()
 fp2.moveToPoint_((110, 100))
 fp2.appendBezierPathWithGlyph_inFont_(68, f)
 
-print intersects(fp1, fp2)
+print( intersects(fp1, fp2) )
 
 # Some other thing inside of the function perhaps?
 
-print intersects(fp2, fp2)
+print( intersects(fp2, fp2) )
 
 p = union(fp1, fp2)
-print p.elementCount()
+print( p.elementCount() )
 
 p =  intersect(fp1, fp2)
-print p.elementCount()
+print( p.elementCount() )
 
 p =  difference(fp1, fp2)
-print p.elementCount()
+print( p.elementCount() )
 
 p = xor(fp1, fp2)
-print p.elementCount()
+print( p.elementCount() )
 
