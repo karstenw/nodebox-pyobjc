f329eb5c1c27c190cdcaf462c843ae3bfc564ae1
Karsten Wolf <karstenwo@web.de>
2025-07-17 11:51:16 +0200

    Update Example fontfamilies 1.py

--- examples/New Functions/fonts/Example fontfamilies 1.py
+++ examples/New Functions/fonts/Example fontfamilies 1.py
@@ -11,10 +11,19 @@ keys = list(fontFamilies.keys())
 
 keys.sort()
 
+fontfamiliesCount = 0
+fontMembersCount = 0
+
 for fmname in keys:
+    fontfamiliesCount += 1
     print( fmname )
     famMembers = fontFamilies[fmname]
     for member in famMembers:
+        fontMembersCount += 1
         print( "    ", member )
         # print "        ", fontFamilies[fmname][member]
     print()
+
+print( "fontfamiliesCount:", fontfamiliesCount )
+print( "fontMembersCount:", fontMembersCount )
+
