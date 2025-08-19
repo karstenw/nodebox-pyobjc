eb1036603878a26a399ac8ec2e73391189cf00fa
Karsten Wolf <karstenwo@web.de>
2025-08-19 17:38:34 +0200

    Update Unicode chars.py

--- examples/New Functions/Unicode chars.py
+++ examples/New Functions/Unicode chars.py
@@ -1,7 +1,9 @@
 
-from __future__ import print_function
+
 
 size(1200, 800)
+
+frames = True
 
 fonts = fontnames()
 f = choice( fonts )
@@ -16,13 +18,8 @@ strokewidth(1)
 transform(CORNER)
 
 # unicode from http://leancrew.com/all-this/2017/06/live-and-let-diaeresis/
+t1 = "This is Sp\u0131n\u0308al Tap!"
 
-# The u"" notation is needed for unicode expansions
-t1 = u"This is Sp\u0131n\u0308al Tap!"
-
-
-# WARNING: This is not your standard Python!
-# no expansions, u"" not needed; 
 t2 = "This is Spın̈al Tap!"
 
 # you dont want this
@@ -34,7 +31,8 @@ dx, dy, t1w, t1h = alltextmetrics(t1, fontsize=fsize, font=f)
 text(t1, x+dx, y+dy)
 
 nofill()
-rect(x, y-fsize, t1w+dx, t1h+dy)
+if frames:
+    rect(x, y-fsize, t1w+dx, t1h+dy)
 
 
 y = 200
@@ -43,7 +41,8 @@ dx, dy, t2w, t2h = alltextmetrics(t2, fontsize=fsize, font=f)
 text(t2, x+dx, y+dy)
 
 nofill()
-rect(x, y-fsize, t2w+dx, t2h+dy)
+if frames:
+    rect(x, y-fsize, t2w+dx, t2h+dy)
 
 y = 300
 fill(0)
@@ -51,7 +50,8 @@ dx, dy, t3w, t3h = alltextmetrics(t3, fontsize=fsize, font=f)
 text(t3, x+dx, y+dy)
 
 nofill()
-rect(x, y-fsize, t3w+dx, t3h+dy)
+if frames:
+    rect(x, y-fsize, t3w+dx, t3h+dy)
 
 
 # This script documents an error in unicode text display.
