542aa5a1a262e774b9fd31db892798f622dd0bf4
Karsten Wolf <karstenwo@web.de>
2026-06-05 14:45:11 +0200

    Create __init__.py

---
+++ nodebox/util/MP4Support/__init__.py
@@ -0,0 +1,202 @@
+import os
+import tempfile
+import pprint
+import Foundation
+import imageio_ffmpeg
+import AppKit
+
+pp=pprint.pprint
+
+NSNumber = Foundation.NSNumber
+
+NSImage = AppKit.NSImage
+NSApplication = AppKit.NSApplication
+NSColor = AppKit.NSColor
+NSData = AppKit.NSData
+NSBitmapImageRep = AppKit.NSBitmapImageRep
+NSJPEGFileType = AppKit.NSJPEGFileType
+
+# 
+# file extensions for later use - extracted via "ffmpeg -formats" and manually selected & sorted; ignored imageformats for now
+#
+# this needs expansion
+
+videoextensions = ("avi caf dhav dvd h261 h263 h264 mkv webm mov mp4 m4a 3gp 3g2 mj2 mpeg ogv rm vcd vob")
+
+audioextensione = ("aa aac ac3 act afc aiff aif aix alaw au ast boa mp2 mp3 oga ogg")
+
+
+class MovieReader:
+    def __init__(self, path, pix_fmt="rgb24", bpp=None, input_params=None, output_params=None, bits_per_pixel=None ):
+        self.path = os.path.abspath( os.path.expanduser(path) )
+        self.pix_fmt = pix_fmt
+        self.bpp = bpp
+        self.input_params = input_params
+        self.output_params = output_params
+        self.bits_per_pixel = bits_per_pixel
+        meta = self.readMeta()
+        if 0:
+            pp(meta)
+        self.fps = meta.get('fps', 0.0)
+        self.audio_codec = meta.get('audio_codec', None)
+        self.codec = meta.get('codec', None)
+        self.duration = meta.get('duration', 0.0)
+        self.pix_fmt = meta.get('pix_fmt', None)
+        self.rotate = meta.get('rotate', 0)
+        self.size = meta.get('size', None)
+        self.source_size = meta.get('source_size', self.size)
+            
+    def openmovie(self):
+        return imageio_ffmpeg.read_frames( self.path, pix_fmt=self.pix_fmt, bpp=self.bpp,
+                                           input_params=self.input_params, output_params=self.output_params,
+                                           bits_per_pixel=self.bits_per_pixel)
+
+
+    def readMeta( self ):
+        movie = self.openmovie()
+        meta = next(movie)
+        movie.close()
+        return meta
+
+
+    def readFrames(self):
+        movie = self.openmovie()
+        
+        # read meta
+        _ = next(movie)
+        
+        for frame in next(movie):
+            yield frame
+        
+        movie.close()
+
+
+
+
+class MovieWriter:
+    def __init__(self, path, size,
+                       pix_fmt_in="rgb24", pix_fmt_out="yuv420p", fps=16, quality=5,
+                       bitrate=None, codec=None, macro_block_size=16, ffmpeg_log_level="warning",
+                       ffmpeg_timeout=None, input_params=None, output_params=None,
+                       audio_path=None, audio_codec=None ):
+
+        self.path = path
+        self.size = size
+        self.pix_fmt_in = pix_fmt_in
+        self.pix_fmt_out = pix_fmt_out
+        self.fps = fps
+        self.quality = quality
+        self.bitrate = bitrate
+        self.codec = codec
+        self.macro_block_size = macro_block_size
+        self.ffmpeg_log_level = ffmpeg_log_level
+        self.ffmpeg_timeout = ffmpeg_timeout
+        self.input_params = input_params
+        self.output_params = output_params
+        self.audio_path = audio_path
+        self.audio_codec = audio_codec
+        
+    def openmovie(self):
+        movie = imageio_ffmpeg.write_frames(
+                self.path,
+                self.size,
+                self.pix_fmt_in,
+                self.pix_fmt_out,
+                self.fps,
+                self.quality,
+                self.bitrate,
+                self.codec,
+                self.macro_block_size,
+                self.ffmpeg_log_level,
+                self.ffmpeg_timeout,
+                self.input_params,
+                self.output_params,
+                self.audio_path,
+                self.audio_codec)
+        return movie
+
+
+    def writeFrames(self, frames):
+        movie = self.openmovie()
+        
+        for frame in frames:
+            movie.send( frame )
+        movie.close()
+
+
+
+
+class Movie(object):
+
+    def __init__(self, fname, fps=30):
+        self.fname = fname
+
+        self.frame = 1
+        self.tmpfname = None
+        self.firstFrame = True
+        self.movie = None
+        self.fps = fps
+        self._time = 0 
+
+
+    def add(self, canvas_or_context):
+        if self.movie is None:
+            # The first frame will be written to a temporary png file,
+            # then opened as a movie file, then saved again as a movie.
+            handle, self.tmpfname = tempfile.mkstemp('.tiff')
+            canvas_or_context.save(self.tmpfname)
+            try:
+                movie, err = QTMovie.movieWithFile_error_(self.tmpfname, None)
+                movie.setAttribute_forKey_(NSNumber.numberWithBool_(True), QTMovieEditableAttribute)
+                range = QTMakeTimeRange(QTMakeTime(0,600), movie.duration())
+                movie.scaleSegment_newDuration_(range, self._time)
+                if err is not None:
+                    raise str(err)
+                movie.writeToFile_withAttributes_(self.fname, {QTMovieFlatten:True})
+                self.movie, err = QTMovie.movieWithFile_error_(self.fname, None)
+                self.movie.setAttribute_forKey_(NSNumber.numberWithBool_(True), QTMovieEditableAttribute)
+                if err is not None:
+                    raise str(err)
+                self.imageTrack = self.movie.tracks()[0]
+            finally:
+                os.remove(self.tmpfname)
+        else:
+            try:
+                canvas_or_context.save(self.tmpfname)
+                img = NSImage.alloc().initByReferencingFile_(self.tmpfname)
+                self.imageTrack.addImage_forDuration_withAttributes_(img, self._time, {QTAddImageCodecType:'tiff'})
+            finally:
+                try:
+                    os.remove(self.tmpfname)
+                except OSError as err:
+                    print(err)
+                    # pass
+        self.frame += 1
+                
+    def save(self):
+        self.movie.updateMovieFile()
+
+
+
+def test():
+    import sys
+    sys.path.insert(0, '../..')
+    sys.path.insert(0, '../../..')
+    from nodebox.graphics import Context # Canvas, 
+    from math import sin
+
+    NSApplication.sharedApplication().activateIgnoringOtherApps_(0)
+    w, h = 512, 320
+    mw = MovieWriter("xx3.mp4", (w,h) )
+    writer = mw.openmovie()
+    for i in range(200):
+        print("Frame %i" % i)
+        ctx = Context()
+        ctx.size(w, h)
+        ctx.rect(100.0+sin(i/10.0)*100.0,i/2.0,100,100)
+        ctx.text(str(i), i*2, 200)
+        writer.send( ctx )
+    m.save()
+    
+if __name__=='__main__':
+    test()
